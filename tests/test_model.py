fcb41402e2e26ea71061486310376a76e652789b
Rakesh kumar <rakeshkumar93694@gmail.com>
2025-04-09 10:14:05 +0530

    add model performance test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,9 @@
 import unittest
 import mlflow
 import os
+import pickle
+import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 class TestModelLoading(unittest.TestCase):
 
@@ -22,10 +25,16 @@ class TestModelLoading(unittest.TestCase):
         mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
         # Load the model from MLflow model registry
-        cls.model_name = "Rakesh_kgpian"
-        cls.model_version = cls.get_latest_model_version(cls.model_name)
-        cls.model_uri = f'models:/{cls.model_name}/{cls.model_version}'
-        cls.model = mlflow.pyfunc.load_model(cls.model_uri)
+        cls.new_model_name = "Rakesh_kgpian"
+        cls.new_model_version = cls.get_latest_model_version(cls.model_name)
+        cls.new_model_uri = f'models:/{cls.model_name}/{cls.model_version}'
+        cls.new_model = mlflow.pyfunc.load_model(cls.model_uri)
+
+        # Load the vectorizer
+        cls.vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
+
+        # Load holdout test data
+        cls.holdout_data = pd.read_csv('data/processed/test_bow.csv')
 
     @staticmethod
     def get_latest_model_version(model_name):
@@ -36,9 +45,36 @@ class TestModelLoading(unittest.TestCase):
     def test_model_loaded_properly(self):
         self.assertIsNotNone(self.model)
 
+    def test_model_performance(self):
+        # Extract features and labels from holdout test data
+        X_holdout = self.holdout_data.iloc[:,0,-1]
+        y_holdout = self.holdout_data.iloc[:,-1]
+
+        # Predict using the new model
+        y_pred_new = self.new_model.predict(X_holdout)
+
+        # Calculate performance metrics for the new_model
+        accuracy_new = accuracy_score(y_holdout, y_pred_new)
+        precision_new = precision_score(y_holdout, y_pred_new)
+        recall_new = recall_score(y_holdout, y_pred_new)
+        f1_new = f1_score(y_holdout, y_pred_new)
+
+        # Define expected thresholds for the performance metrics
+        expected_accuracy = 0.70
+        expected_precision = 0.70
+        expected_recall = 0.70
+        expected_f1 = 0.70
+
+        # Assert that the new model meets the performance thresholds
+        self.assertGreaterEqual(accuracy_new, expected_accuracy, f'Accuracy should be at least {expected_accuracy}') 
+        self.assertGreaterEqual(precision_new, expected_precision, f'Prediction should be at least {expected_precision}')
+        self.assertGreaterEqual(recall_new, expected_recall, f'Recall should be at least {expected_recall}')
+        self.assertGreaterEqual(expected_f1, f'F1 score should be at least {expected_f1}')
+
 if __name__ == "__main__":
     unittest.main()
 
 
-        
+
+
 
